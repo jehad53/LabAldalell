2afffaafc7ee3bf3050b0e055e3cd467c81ea2ee
Jehad-Eljrochi <jehad.eljrocih@gmail.com>
2025-12-05 21:26:20 +0200

    Edit src componets

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import { Helmet } from 'react-helmet-async';
 import schema from '../../data/schema.json';
 
 const Layout = ({ children }) => {
     return (
         <div className="flex flex-col min-h-screen font-sans bg-gray-50 text-right" dir="rtl">
-            <Helmet>
-                <script type="application/ld+json">{JSON.stringify(schema)}</script>
-            </Helmet>
+            {/* تم حذف Helmet لأنه غير موجود */}
             <Header />
             <main className="flex-grow">
                 {children}
